9dd03ab865a2fb587407a1a6619c96503687321d
dcristobal19 <diecristher@gmail.com>
2019-04-24 13:36:43 +0200

    instalacion de php ide helper mediante los comandos de la pagina https://github.com/barryvdh/laravel-ide-helper

--- learning/app/Role.php
+++ learning/app/Role.php
@@ -4,6 +4,14 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Role
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Role query()
+ * @mixin \Eloquent
+ */
 class Role extends Model
 {
     const ADMIN = 1;
