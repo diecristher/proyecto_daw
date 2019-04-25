fae8dddc69c60fc8d610f729a0f801ac4475e422
dcristobal19 <diecristher@gmail.com>
2019-04-25 11:20:18 +0200

    Utilizo transacciones para procesar el registro y el inicio de sesion

--- learning/app/Http/Controllers/Auth/LoginController.php
+++ learning/app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
 {
@@ -41,7 +42,45 @@ class LoginController extends Controller
         return Socialite::driver($driver)->redirect();
     }
     public function handleProviderCallback (string $driver) {
-            $socialUser = Socialite::driver($driver)->user();
-            dd($socialUser);
+    if( ! request()->has('code') || request()->has('denied')) {
+                    session()->flash('message', ['danger', __("Inicio de sesión cancelado")]);
+                    return redirect('login');
                 }
+                $socialUser = Socialite::driver($driver)->user();
+
+                        $user = null;
+                        $success = true;
+                        $email = $socialUser->email;
+                        $check = User::whereEmail($email)->first();
+                        if($check) {
+                                $user = $check;
+                            } else {
+                                \DB::beginTransaction();
+                                try {
+                                        $user = User::create([
+                                                "name" => $socialUser->name,
+                                                    "email" => $email
+                                            ]);
+                                        UserSocialAccount::create([
+                                                "user_id" => $user->id,
+                                                    "provider" => $driver,
+                                                    "provider_uid" => $socialUser->id
+                                            ]);
+                                        Student::create([
+                                                "user_id" => $user->id
+                                            ]);
+                                    } catch (\Exception $exception) {
+                                                $success = $exception->getMessage();
+                                                \DB::rollBack();
+                                    }
+                            }
+
+                            if($success === true) {
+                                \DB::commit();
+                                auth()->loginUsingId($user->id);
+                                return redirect(route('home'));
+                            }
+                            session()->flash('message', ['danger', $success]);
+                        return redirect('login');
+}
 }
--- learning/config/services.php
+++ learning/config/services.php
@@ -49,6 +49,6 @@ return [
             'client_id' => env('FACEBOOK_CLIENT_ID'),
             'client_secret' => env('FACEBOOK_CLIENT_SECRET'),
             'redirect' => '/login/facebook/callback',
-],
-],
+
+],],
 ];
